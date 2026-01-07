7caa230ec70844da4bfbcc611960a7c5d8c284b8
Eddie Johansson <eddie.johanssonn@hotmail.com>
2026-01-07 15:50:54 +0100

    ändring av user

--- app/config/centralbank.php
+++ app/config/centralbank.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 
 return [
     'base_url' => 'https://www.yrgopelag.se/centralbank',
-    'user'     => 'SpookyHotel',
+    'user'     => 'Eddie',
     'api_key'  => $_ENV['API_KEY'],
 ];
